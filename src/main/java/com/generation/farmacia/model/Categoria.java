ef8c42a3ff655ed820c71e9e23cdfa3516e45676
Lucky2109 <lukao2109@gmail.com>
2024-01-31 10:22:54 -0300

    CRUD-Categoria 31/01/2024

---
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,61 @@
+package com.generation.farmacia.model;
+
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Size(min = 2, max = 255, message = "A Categoria deve ter no mínimo 2 caracteres e no máximo 255 caracteres")
+	@NotBlank (message = "Atributo nomeCategoria é obrigatório")
+	private String nomeCategoria;
+	
+	@Size(min = 10, max = 255, message = "A Descrição deve ter no mínimo 10 caracteres e no máximo 255 caracteres")
+	@NotBlank(message = "Atributo descricaoCategoria é obrigatório")
+	private String descricaoCategoria;
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomeCategoria() {
+		return nomeCategoria;
+	}
+
+	public void setNomeCategoria(String nomeCategoria) {
+		this.nomeCategoria = nomeCategoria;
+	}
+
+	public String getDescricaoCategoria() {
+		return descricaoCategoria;
+	}
+
+	public void setDescricaoCategoria(String descricaoCategoria) {
+		this.descricaoCategoria = descricaoCategoria;
+	}
+
+
+
+}
